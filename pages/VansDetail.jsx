284c6414f9c74d0dbc7d7b12cdfad9fde4e54516
ienashr <ienasharisah@gmail.com>
2023-04-07 20:00:48 -0700

    Adding params for the detail page

--- pages/VansDetail.jsx
+++ pages/VansDetail.jsx
@@ -3,15 +3,26 @@ import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
     const params = useParams()
-    const [vans, setVans] = React.useState([])
+    const [van, setVans] = React.useState('')
 
         React.useEffect(() => {
-            fetch("/api/vans/:vanid")
+            fetch(`/api/vans/${params.id}`)
                 .then(res => res.json())
                 .then(data => setVans(data.vans))
-        }, [])
+        }, [params.id])
  
-        console.log(vans)
-
-    return <h1>Van detail page goes here</h1>
+    return (
+        <div className="van-detail-container">
+        {van ? (
+            <div className="van-detail">
+                <img src={van.imageUrl} />
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                <h2>{van.name}</h2>
+                <p className="van-price"><span>${van.price}</span>/day</p>
+                <p>{van.description}</p>
+                <button className="link-button">Rent this van</button>
+            </div>
+        ) : <h2>Loading...</h2>}
+    </div>
+    )
 }
